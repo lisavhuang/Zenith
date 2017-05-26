cf49e1fab48301a2ee4570401d0b708a9adf6643
“Lisa <lisavhuang99@gmail.com>
2017-05-26 16:44:35 -0700

    theres a button that doesn't save stuff

--- app/src/main/java/com/example/zenith/MainActivity.java
+++ app/src/main/java/com/example/zenith/MainActivity.java
@@ -6,14 +6,26 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends Activity {
 
     private TextView mTextMessage;
     ProgressBar prg;
+    EditText editText;
+    TextView textView;
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -46,7 +58,43 @@ public class MainActivity extends Activity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         prg = (ProgressBar) findViewById(R.id.progressBar1);
+        editText = (EditText)findViewById((R.id.task));
+        textView = (TextView) findViewById(R.id.viewtaskbutton);
 
+
+    }
+
+    public void submitTask(View view){
+        String Message = editText.getText().toString();
+        String filename = "data";
+        try {
+            FileOutputStream fileOutputStream = openFileOutput(filename, MODE_PRIVATE);
+            fileOutputStream.write(Message.getBytes());
+            fileOutputStream.close();
+            Toast.makeText(getApplicationContext(),"Task Created", Toast.LENGTH_LONG).show();
+        } catch( FileNotFoundException e ){
+            e.printStackTrace();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void viewTask(View view){
+
+        try {
+            String Task;
+            FileInputStream fileInputStream = openFileInput("data");
+            InputStreamReader inputStreamReader= new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuffer stringBuffer = new StringBuffer();
+            while((Task = bufferedReader.readLine())!=null){
+                stringBuffer.append(Task + "\n");
+            }
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+        } catch( IOException e){
+            e.printStackTrace();
+        }
     }
 
 }
